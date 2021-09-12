4f2019454c5acf3452e0414b278bd349849c6598
Prelipcean Cristian <prelipceancristian100@gmail.com>
2021-09-12 21:56:57 +0300

    NavigationDrawerContent front, routes and constants

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app style="background-color: rgba(46,64,87);">
-    <v-app-bar app :color="constants.mainBlue" dark>
+    <v-app-bar app dark :color="constants.mainBlue">
       <v-btn icon @click="isNavigationOpen = !isNavigationOpen">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
@@ -8,11 +8,18 @@
       <v-spacer></v-spacer>
       <User :user="testUser"></User>
     </v-app-bar>
-    <v-navigation-drawer app v-model="isNavigationOpen">
+    <v-navigation-drawer
+      app
+      v-model="isNavigationOpen"
+      width="300"
+      dark
+      temporary
+      :color="constants.black"
+    >
+      <NavigationDrawerContent />
     </v-navigation-drawer>
-    <v-main :color="black">
+    <v-main>
       <Tiles />
-      
     </v-main>
   </v-app>
 </template>
@@ -21,13 +28,15 @@
 import Tiles from './components/Tiles'
 import { constants } from './common/constants'
 import User from './components/User'
+import NavigationDrawerContent from './components/NavigationDrawerContent'
 
 export default {
   name: 'App',
 
   components: {
     Tiles,
-    User
+    User,
+    NavigationDrawerContent
   },
 
   data: () => ({
--- src/common/constants.js
+++ src/common/constants.js
@@ -5,5 +5,8 @@ export const constants = {
   sage: '#aca885',
   charcoalBlue: '#2e4057',
   whitesmoke: '#f5f5f5',
+  richBlack: '#011627',
+  darkOrange: '#f34213',
+  black: '#2f3136'
 }
 
